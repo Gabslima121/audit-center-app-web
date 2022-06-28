b3343651e0cd690a868590774a204dd6516286d8
Gabslima121 <glima@vixting.com.br>
2022-06-27 23:34:22 -0300

    finished header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,23 @@
+import { SignOut, UserRectangle } from 'phosphor-react'
+import { Link } from 'react-router-dom'
 import logoImg from '../../assets/logo-redondo.svg'
 
 export function Header() {
   return (
-    <div className='border-2 border-black'>
-      <img src={logoImg} />
+    <div className="bg-white rounded-3xl p-3">
+      <div className="float-right">
+        <div className="flex flex-row mt-2 flex-auto">
+          <Link to="/my-profile" className="my-2 mx-2">
+            <UserRectangle size={25} color="#030303" className="float-left" />
+            <p className="ml-8">Meu Perfil</p>
+          </Link>
+
+          <Link to="/" className="my-2 mx-2">
+            <SignOut size={25} color="#030303" className="float-left" />
+            <p className="ml-8">Sair</p>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -16,12 +16,12 @@ export function Menu() {
 
       {isAdmin && isSuperAdmin && (
         <Link to="/companys" className="my-2">
-          <Buildings size={24} color="#030303" className="ml-2 float-left"/>
+          <Buildings size={25} color="#030303" className="ml-2 float-left" />
           <p className="ml-10 mt-px">Empresas</p>
         </Link>
       )}
 
-      <Link to="/graphs">
+      <Link to="/graphs" className="my-2">
         <ChartBar size={25} className="ml-2 float-left" />
         <p className="ml-10 mt-px">Gráficos</p>
       </Link>
